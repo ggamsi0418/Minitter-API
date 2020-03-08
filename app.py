280353600bded02ce9696945e863dfa2469ab357
ggamsi0418 <58096698+ggamsi0418@users.noreply.github.com>
2020-03-09 05:49:40 +0900

    add follow endpoint and test request

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
+from flask.json import JSONEncoder
+from functools import wraps
+import json
+
 
 app = Flask(__name__)
 app.id_count = 1
 app.users = {}
+app.tweets = []  # 사용자들의 트윗을 저장할 디렉터리
+
+
+def json_api(f):
+    @wraps(f)
+    def inner(*args, **kwds):
+        r = f(*args, **kwds)
+        result = make_response(json.dumps(
+            r, ensure_ascii=False))
+        return result
+    return inner
 
 
 @app.route("/ping", methods=['GET'])
 def ping():
-
     return "pong"
 
 
 @app.route("/sign-up", methods=['POST'])
+@json_api
 def sign_up():
     new_user = request.json
     new_user["id"] = app.id_count
     app.users[app.id_count] = new_user
     app.id_count += 1
 
-    return jsonify(new_user)
-
-
-app.tweets = []  # 사용자들의 트윗을 저장할 디렉터리
+    return new_user
 
 
 @app.route('/tweet', methods=['POST'])
@@ -42,3 +54,20 @@ def tweet():
     })
 
     return '', 200
+
+
+@app.route('/follow', methods=['POST'])
+@json_api
+def follow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['follow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return '사용자가 존재하지 않습니다.', 400
+
+    user = app.users[user_id]
+    user.setdefault('follow', set()).add(user_id_to_follow)
+    user['follow'] = list(user['follow'])
+
+    return user
